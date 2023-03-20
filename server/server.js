0ddcb33bef8ba6e51fe4fb132832ed73720740fc
HangFamily <hangquoctai.family@gmail.com>
2023-03-20 16:04:57 +0700

    Fix dotenv

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import path from "path";
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+import dotenv from "dotenv";
+dotenv.config();
 
 import express from "express";
 import cors from "cors";
